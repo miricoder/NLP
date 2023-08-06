46f78fe5372bac571922df7df6760509ad3a4fdf
Mirali Mirzayev <miralimirzayev@Miralis-MacBook-Pro.local>
2023-08-05 20:50:35 -0400

    Saving Help Functions to see if method works 4

--- helper_functions.py
+++ helper_functions.py
@@ -289,19 +289,19 @@ def calculate_results(y_true, y_pred):
   return model_results
 
 def show_wordcloud(data, title, parameter):
-  """
-  For Visualizing Word Frequcueny in WordCloud
+"""
+For Visualizing Word Frequcueny in WordCloud
 
-  Args:
-    data: Column to visulalize (parameter)
-    title: Title of the WorkCloud Visualized
-  """
+Args:
+  data: Column to visulalize (parameter)
+  title: Title of the WorkCloud Visualized
+"""
   text = ' '.join(data['text'].astype(str).tolist())                 # Converting Summary column into list
   stopwords = set(wordcloud.STOPWORDS)                                  # instantiate the stopwords from wordcloud
-    
+  
   fig_wordcloud = wordcloud.WordCloud(stopwords=stopwords,background_color='white',          # Setting the different parameter of stopwords
-                    colormap='viridis', width=800, height=600).generate(text)
-    
+                  colormap='viridis', width=800, height=600).generate(text)
+  
   plt.figure(figsize=(14,11), frameon=True)                             
   plt.imshow(fig_wordcloud)  
   plt.axis('off')
